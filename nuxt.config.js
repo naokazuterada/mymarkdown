f60305a9d13ac423bfc32f7616f26688ab351c54
Naokazu Terada <naokazu.terada@gmail.com>
2018-08-17 00:13:58 +0900

    Set firebase script in head

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,19 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    secript: [
+      { src: 'https://www.gstatic.com/firebasejs/5.3.1/firebase.js' },
+      { innerHTML: '// Initialize Firebase\
+                    var config = {\
+                      apiKey: "AIzaSyD2vMqBxij3DiZ4NAbhbzTYaOiylOddyi0",\
+                      authDomain: "mymarkdown-dee5f.firebaseapp.com",\
+                      databaseURL: "https://mymarkdown-dee5f.firebaseio.com",\
+                      projectId: "mymarkdown-dee5f",\
+                      storageBucket: "",\
+                      messagingSenderId: "164321246414"\
+                    };\
+                    firebase.initializeApp(config);' }
     ]
   },
   mode: 'spa',
